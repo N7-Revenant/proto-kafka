6abbb722a06f637680ed967784628347cbe275f6
dmitriy <dmitri.zol@rambler.ru>
2021-10-21 19:45:27 +0300

    kafka-python producer example added

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 Kafka interaction prototype - Тестовое окружение для развертывания Kafka и набор прототипов, представляющих пример организации взаимодействия с Kafka из Python
 
- * aiokafka_producer.py - пример реализации Producer'а для Kafka с использованием библиотеки aiokafka
- * aiokafka_consumer.py - пример реализации Consumer'а для Kafka с использованием библиотеки aiokafka
+ * aiokafka/example/producer.py - пример реализации Producer'а для Kafka с использованием библиотеки aiokafka
+ * aiokafka/example/consumer.py - пример реализации Consumer'а для Kafka с использованием библиотеки aiokafka
+ * kafka-python/example/producer.py - пример реализации Producer'а для Kafka с использованием библиотеки kafka-python
---
+++ kafka-python/example/producer.py
@@ -0,0 +1,47 @@
+from kafka import KafkaProducer
+from kafka.producer.future import RecordMetadata
+
+import asyncio
+
+
+async def work():
+    def on_send_success(record_metadata: RecordMetadata):
+        print(f"Successfully sent message to topic: {record_metadata.topic}!")
+
+    def on_send_error(e: Exception):
+        print(f"Error: {type(e)}:{e}")
+
+    producer = KafkaProducer(bootstrap_servers=['192.168.50.71:9092'])
+    message = "My message"
+    print("Working!")
+
+    while True:
+        producer.send("MyTopic", message.encode()).add_callback(on_send_success).add_errback(on_send_error)
+        try:
+            await asyncio.sleep(1)
+
+        except asyncio.CancelledError:
+            break
+
+        except Exception as exc:
+            print(f"Unexpected Error: {exc}")
+            break
+
+    producer.flush(timeout=3)
+    producer.close()
+
+
+def main():
+    print("Producer started!")
+    loop = asyncio.get_event_loop()
+    task = loop.create_task((work()))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        task.cancel()
+    loop.stop()
+    print("Producer finished!")
+
+
+if __name__ == '__main__':
+    main()
